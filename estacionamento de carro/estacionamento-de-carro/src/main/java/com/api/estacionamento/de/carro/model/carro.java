1de5b8c03780c5d604b045c70f2ae68c5c9f55ca
jeanmaki2002 <jeanmaki2002@gmail.com>
2022-02-16 22:02:37 -0300

    projeto final do bootcamp da dio

---
+++ estacionamento de carro/estacionamento-de-carro/src/main/java/com/api/estacionamento/de/carro/model/carro.java
@@ -0,0 +1,15 @@
+package com.api.estacionamento.de.carro.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+@Entity
+@Table(name="estacionamentos")
+public class carro implements Serializable {
+    private static final long serialVersionVID = 1l;
+
+    @Id
+    @
+}
---
+++ estacionamento de carro/estacionamento-de-carro/src/test/java/com/api/estacionamento/de/carro/EstacionamentoDeCarroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.estacionamento.de.carro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstacionamentoDeCarroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
